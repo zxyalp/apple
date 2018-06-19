9d5664fc718b8d6967e7b43c84d04890ae44ed6d
zxy <orcexzy@163.com>
2018-06-19 18:11:51 +0800

    django demo

--- polls/admin.py
+++ polls/admin.py
@@ -3,12 +3,21 @@ from .models import Question, Choice
 # Register your models here.
 
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']})
     ]
+    inlines = [ChoiceInline]
+
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
+
 
 from .models import Question, Choice
 
@@ -44,17 +46,23 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__late=timezone.now()).order_by('pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 def vote(request, question_id):
